182293a9ba3089a3a67ca8f2749eda56ce9b53b9
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-04 18:02:27 +0300

    adding more things for App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { AiOutlinePlus } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-     ervery thisngs work?
+    <div>
+      <div>
+        <h3>Todo App</h3>
+        <form>
+          <input type="text" placeholder="Add Todo" />
+          <button>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul></ul>
+      </div>
     </div>
   );
 }
