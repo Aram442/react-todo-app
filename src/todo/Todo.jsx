5ed3ea7c843b51ae3dfbcc9677e37acfcb36df4e
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-04 20:21:32 +0300

    Adding Sass file for Todo

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./todo.scss";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 function Todo({ todo }) {
   return (
-    <li>
+    <li className={todo.completed ? "completed" : "li"}>
       <div>
         <input type="checkbox" />
         <p>{todo.text}</p>
