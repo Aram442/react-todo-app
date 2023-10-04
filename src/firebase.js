6a4580f6c8015378252cfed14e91450bb0513564
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-04 19:34:27 +0300

    Creating Firebase File

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCm7c5OmSAuCfoEZHIfkRD5tIifaGpVr2k",
+  authDomain: "react-todo-app-1a193.firebaseapp.com",
+  projectId: "react-todo-app-1a193",
+  storageBucket: "react-todo-app-1a193.appspot.com",
+  messagingSenderId: "51229562137",
+  appId: "1:51229562137:web:be2879e027fcb6e9ebe188",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
