839fdfdb0a7ee5d03033d799c941605e352650f5
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-09 20:25:38 +0300

    adding React icon package

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
+    <div className="shdow-xl mt-32 p-10 text-gray-700">
       <h2>Join Today</h2>
       <div>
         <h3>Sign with one of the providers</h3>
