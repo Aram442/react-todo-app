609d248748b4772b82865c76edeb8b20626a4132
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-09 20:40:06 +0300

    Adding Some Style for Login Buttons

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { AiFillFacebook } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
 
 function Login() {
   return (
-    <div className="shdow-xl mt-32 p-10 text-gray-700">
-      <h2>Join Today</h2>
-      <div>
-        <h3>Sign with one of the providers</h3>
+    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+      <h2 className="text-3xl font-medium">Join Today</h2>
+      <div className="py-4">
+        <h3 className="py-4">Sign with one of the providers</h3>
       </div>
-      <div>
-        <button>Sign in with Google</button>
-        <button>Sign with Facebook</button>
+      <div className="flex flex-col gap-3">
+        <button className="text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2">
+          <FcGoogle className="text-2xl" /> Sign in with Google
+        </button>
+        <button className="text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2">
+          <AiFillFacebook className="text-2xl" /> Sign with Facebook
+        </button>
       </div>
     </div>
   );
