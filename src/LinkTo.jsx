074a5b9f1b51f3b49c5808518c8d7974cb5acceb
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-09 22:01:52 +0300

    adding Image if User Exiest

--- src/LinkTo.jsx
+++ src/LinkTo.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-
+import { auth } from "./firebase";
 
 function LinkTo() {
+  const [user, loading] = useAuthState(auth);
   return (
     <div>
-      <nav className="flex justify-between items">
+      <nav className="flex justify-between items-center py-10">
         <Link to={"/"}>Logo</Link>
         <ul>
           <li>
-            <Link to="/auth/login" className="py-2 px-4 text-lg">
-              join now
-            </Link>
+            {!user && (
+              <Link
+                to="/auth/login"
+                className="py-2 px-4 text-lg bg-teal-500 text-white rounded-lg font-medium ml-8"
+              >
+                join now
+              </Link>
+            )}
+            {user && (
+              <div>
+                <Link to={"/dashboard"}>
+                  <img
+                    src={user.photoURL}
+                    alt="user image"
+                    referrerPolicy="no-referrer"
+                    className="w-12 rounded-full"
+                  />
+                </Link>
+              </div>
+            )}
           </li>
         </ul>
       </nav>
