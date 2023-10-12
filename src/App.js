9d5057c14c1e464ca7c0a5d44491be3b54ef2701
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-12 20:14:57 +0300

    finishing SignOut function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { AiOutlinePlus } from "react-icons/ai";
 import Login from "./auth/Login.jsx";
 import AddTodos from "./AddTodos.jsx";
 import SignUp from "./auth/SignUp.jsx";
+import AuthDatails from "./auth/AuthDatails.jsx";
 
 function App() {
   return (
     <div>
       <Login />
       <SignUp />
+      <AuthDatails />
       <AddTodos />
     </div>
   );
--- src/auth/AuthDatails.jsx
+++ src/auth/AuthDatails.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
 function AuthDatails() {
   const [AuthUser, setAuthUser] = useState(null);
 
   useEffect(() => {
+    //----------------------- SIGN OUT ---------------------//
     const listen = onAuthStateChanged(auth, (user) => {
       if (user) {
         setAuthUser(user);
@@ -13,8 +14,33 @@ function AuthDatails() {
         setAuthUser(null);
       }
     });
+    //----------------- WORKED TO SIGN OUT ----------------//
+    return () => {
+      listen();
+    };
   }, []);
-  return <div>{AuthUser ? <p>Signed In</p> : <p>Sugned Out</p>}</div>;
+
+  //----------------------- SIGN OUT ---------------------//
+  const userSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Sign out Successful");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      {AuthUser ? (
+        <>
+          <p>`Signed In as {AuthUser.email}`</p>
+          <button onClick={userSignOut}>Sign Out</button>
+        </>
+      ) : (
+        <p>Sugned Out</p>
+      )}
+    </div>
+  );
 }
 
 export default AuthDatails;
